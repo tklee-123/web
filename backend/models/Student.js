10cdd1d978baac5a925df9a734e4980f5cba6dbd
tklee-123 <taly24061405@gmail.com>
2023-12-25 21:38:20 +0700

    takhanhly

--- backend/models/Student.js
+++ backend/models/Student.js
@@ -1,20 +1,28 @@
 const mongoose = require('mongoose');
 
 // Thêm useNewUrlParser và useUnifiedTopology vào options
+mongoose.connect('mongodb://0.0.0.0:27017/web', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 const studentSchema = new mongoose.Schema({
     _id: {
         type: String,
         require: true
     },
-    firstname: {
+    name: {
         type: String,
         require: true
     },
-    lastname: {
+    birthday: {
         type: String,
         require: true
     },
-    address: {
+    sex: {
+        type: String,
+        require: true
+    },
+    field: {
         type: String,
         require: true
     },
@@ -22,9 +30,20 @@ const studentSchema = new mongoose.Schema({
         type: String,
         require: true
     },
-    gpa: {
+    email: {
+        type: String,
+        require: true
+    },
+    phone: {
+        type: String,
+        require: true
+    },
+    cpa: {
         type: Number,
         require: true
+    },
+    cert: {
+        type: Number
     }
 });
 const Students = mongoose.model("Student", studentSchema);
